4bac8198518315eded2de95adff27faa2880b592
Corina Hancco <chanccov@unsa.edu.pe>
2021-05-10 03:59:44 -0500

    listar archivos

--- pub/scripts/ajaxCliente.js
+++ pub/scripts/ajaxCliente.js
@@ -1,3 +1,13 @@
 // 1. Listar los archivos Markdown disponibles
+function listar(){
+    const url = "http://localhost:3000/listar";
+    console.log(url);
+  
+    fetch(url)
+    .then((response) => response.json())
+    .then((files) => {
+      document.querySelector("#lista").innerHTML = files;
+    });
+}
 // 2. Ver el contenido Markdown traducido a HTML
 // 3. Crear nuevos archivos MarkDown y almacenarlos en el servidor
--- serverMarkDown.js
+++ serverMarkDown.js
@@ -11,4 +11,21 @@ app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.get("/listar", (request, response) => {
+	fs.readdir(path.resolve(__dirname, "pub/"),(err, files) => {
+		if (err) {
+		  console.error(err);
+		  response.status(500).json({
+			error: "message",
+		  });
+		  return;
+		}
+		console.log(files);
+		response.json(files);
+		//response.text(files);
+		/*response.json({
+		  text: data.replace(/,/g, "<br>"),
+		});*/
+		});
+	});
 
